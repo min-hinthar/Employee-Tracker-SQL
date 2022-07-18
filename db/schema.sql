9aca75609c5f576dba87c69c86e0a597b2fc2f6b
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2022-07-17 22:31:34 -0700

    spelling error fixes

--- db/schema.sql
+++ db/schema.sql
@@ -14,11 +14,11 @@ CREATE TABLE departments (
 
 -- table for roles
 CREATE TABLE roles (
-    roles_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    role_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT,
-    FOREIGN KEY (department_id) REFERENCES departments(department_id) ON DELETE SET NULL
+    FOREIGN KEY (department_id) REFERENCES departments(department_id)
 );
 
 -- table for employees
@@ -28,5 +28,6 @@ CREATE TABLE employees (
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES roles(role_id)
     FOREIGN KEY (manager_id) REFERENCES employees(employee_id) ON DELETE SET NULL
 );
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+-- use the employee_DB to insert employee information
+USE employee_DB;
+
+-- departments table values Sales, Engineering, Finance, Legal
+INSERT INTO departments (department_name)
+VALUES ('Sales', 'Engineering', 'Finance', 'Legal');
+
+-- roles table values Title, Salary, 
