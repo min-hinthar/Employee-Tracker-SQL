d180f4734b6705d0194bcb88c8db227db7f240b6
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2022-07-17 21:43:28 -0700

    schema.sql updated, emplyee table created with properties id, firstname, lastname, role id , manager id

--- db/schema.sql
+++ db/schema.sql
@@ -7,18 +7,26 @@ USE employee_DB;
 
 -- create 3 tables for dept, role, employee
 -- table for departments
-CREATE TABLE department (
+CREATE TABLE departments (
     department_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     department_name VARCHAR(30)
 );
 
 -- table for roles
-CREATE TABLE role (
-    role_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE roles (
+    roles_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT,
-    FOREIGN KEY (department_id) REFERENCES department_id
+    FOREIGN KEY (department_id) REFERENCES departments(department_id) ON DELETE SET NULL
 );
 
 -- table for employees
+CREATE TABLE employees (
+    employee_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (manager_id) REFERENCES employees(employee_id) ON DELETE SET NULL
+);
