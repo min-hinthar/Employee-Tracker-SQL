833ff298b841c35e8ea67645536ab589546d722a
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2022-07-22 03:08:31 -0700

    viewAllEmployee function updated to console.table view all employees

--- server.js
+++ server.js
@@ -81,8 +81,16 @@ function trackerPrompt () {
 // console.table to view all employess
 function viewAllEmployee() {
     sequelize.query(
-        'SELECT employees.first_name AS First_Name, employees.last_name AS Last_Name, roles.title AS Title, departments.department_name AS Department, CONCAT(manager.first_name, manager.last_name) AS Manager FROM employees LEFT JOIN employees manager on manager_id = employees.manager_id INNER JOIN roles ON (role_id = employees.role_id) INNER JOIN department ON (department_id = roles.department_id) ORDER BY employees.employee_id;')
-}
+        'SELECT employees.first_name AS First_Name, employees.last_name AS Last_Name, roles.title AS Title, departments.department_name AS Department, CONCAT(manager.first_name, manager.last_name) AS Manager FROM employees LEFT JOIN employees manager on manageNER JOIN roles ON (role_id = employees.role_id) INNER JOIN department ON (department_id = roles.department_id) ORDER BY employees.employee_id;').spread(function(err, res) {
+            if (err) throw (err);
+            // view all employees from table
+            console.table(res);
+            console.log("Viewing All Employees...");
+            // return to main prompt
+            trackerPrompt();
+        });
+};
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
